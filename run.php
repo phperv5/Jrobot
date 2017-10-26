3eadf7580db5432c56982888ed80df903a5237b4
iwisunny <iwisunny@gmail.com>
2017-10-26 18:40:49 +0800

    commit

--- run.php
+++ run.php
@@ -9,7 +9,7 @@
 
 date_default_timezone_set('Asia/Shanghai');
 
-require __DIR__ . '/vendor1/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 $config = require_once __DIR__ . '/config.php';
 
 $console = new Kcloze\Bot\Console($config);
